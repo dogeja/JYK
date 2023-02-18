543d720555c6be49c2e454a4b958b0c825425ed1
dogeja <disk6473@gmail.com>
2023-02-18 23:27:47 +0900

    add: 768px and 1024px

--- js/app.js
+++ js/app.js
@@ -1,20 +1,37 @@
 $(document).ready(function () {
+  $(window)
+    .resize(function () {
+      // 데스크탑 스크롤이벤트
+      if (window.innerWidth > 1024) {
+        $(window).scroll(function () {
+          const currentScrollValue = document.documentElement.scrollTop;
+
+          if (currentScrollValue > 2400) {
+            $(".scroll_down").css("opacity", 0);
+          } else {
+            $(".scroll_down").css("opacity", 1);
+          }
+        });
+        // 태블릿 모바일 터치이벤트
+      } else if (window.innerWidth <= 1024) {
+        $("body").on("touchstart", function (e) {
+          const currentTouchValue = e.originalEvent.touches[0].pageY;
+          console.log(currentTouchValue);
+          if (currentTouchValue > 1200) {
+            $(".scroll_down").css("opacity", 0);
+          } else if (currentTouchValue <= 1200) {
+            $(".scroll_down").css("opacity", 1);
+          }
+        });
+      }
+    })
+    .resize();
+  // 리사이즈시 재실행
   $(".intro").show();
   setTimeout(introAnimation, 1200);
   function introAnimation() {
     $(".intro").fadeOut();
   }
-  $("body").on("mousewheel", function () {
-    const currentScrollValue = document.documentElement.scrollTop;
-
-    if (currentScrollValue > 2400) {
-      $(".scroll_down").css("opacity", 0);
-    } else {
-      $(".scroll_down").css("opacity", 1);
-    }
-
-    console.log(currentScrollValue);
-  });
   // animate.css를 aniview에 등록
   $(".aniview").AniView({
     animateClass: "animate__animated",
@@ -29,6 +46,7 @@ $(document).ready(function () {
     window.scrollTo(0, 0);
     $(".scroll_down").css("opacity", 1);
   });
+  // slick , 새로고침시 0
   setTimeout(function () {
     $(".web").slick({
       infinite: true,
